2dd484aaaa01b01f095fd7102d820c05e1e22e46
Thomas Fisher <thomas.fisher1@ntlworld.com>
2017-08-09 17:11:45 +0100

    Basic Movement
    Ship can move along horizontal and vertical axes.

---
+++ Assets/PlayerController.js
@@ -0,0 +1,8 @@
+﻿ #pragma strict 
+ 
+ var speed : float = 1.0;
+ 
+ function Update() {
+     var move = Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+     transform.position += move * speed * Time.deltaTime;
+ }
