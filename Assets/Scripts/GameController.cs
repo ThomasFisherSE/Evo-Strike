7148d3c676d7cdb18ecb711878f91c3898d943e7
Thomas Fisher <thomas.fisher1@ntlworld.com>
2017-08-16 15:55:51 +0100

    Variable Hazards
    Multiple asteroid types added.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,7 +14,7 @@ public class GameController : MonoBehaviour {
     private bool gameOver;
     private bool restart;
 
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
@@ -73,6 +73,7 @@ public class GameController : MonoBehaviour {
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards[Random.Range(0,hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
 
