d6a8a008c59b92890744b21d0eddda9db7b55d6c
Thomas Fisher <thomas.fisher1@ntlworld.com>
2017-08-17 23:27:00 +0100

    Scrolling Background and Enemy Ships
    Added parallaxing background effect.
    
    Added enemy ships that shoot at the player (but don't move yet).

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour {
+    public float scrollSpeed;
+    private float tileSize;
+    private Vector3 startPosition;
+
+	void Start () {
+        startPosition = transform.position;
+        tileSize = transform.localScale.y;
+	}
+	
+	void Update () {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed * -1, tileSize);
+        transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -26,29 +26,30 @@ public class DestroyByContact : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if (gameObject != null)
+        // Do nothing if colliding with boundary
+        //if (other.tag == "Boundary" || other.tag == "Enemy")
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
         {
-            // Do nothing if colliding with boundary
-            if (other.tag == "Boundary")
-            {
-                return;
-            }
+            return;
+        }
 
+        if (explosion != null)
+        {
             // Create an explosion on the GameObject
             Instantiate(explosion, transform.position, transform.rotation);
-
-            // If colliding with a player, create an explosion on the player
-            if (other.tag == "Player")
-            {
-                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
-                gameController.GameOver();
-            }
-
-            gameController.AddScore(scoreValue);
-
-            Destroy(other.gameObject);
-            Destroy(gameObject);
         }
-        
+
+        // If colliding with a player, create an explosion on the player
+        //if (other.tag == "Player")
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+
+        gameController.AddScore(scoreValue);
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
     }
 }
