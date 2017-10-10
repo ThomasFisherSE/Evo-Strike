11a7d9ed245d279d235a9bbcbb48bb96766fa7b9
Thomas Fisher <thomas.fisher1@ntlworld.com>
2017-10-10 14:44:32 +0100

    Added MainMenu Scene
    Added a main menu scene, created a script to load scenes and quit the
    game.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
