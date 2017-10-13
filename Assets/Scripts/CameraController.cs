c1c777fba662f150f62bd67dd49e4b500db39a2a
Thomas Fisher <thomas.fisher1@ntlworld.com>
2017-10-13 11:01:09 +0100

    Added alternative camera view and camera controller
    Can now change the view point by pressing 'v'.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Camera forwardFacingCamera;
+
+    private Queue<Camera> cameraQueue;
+
+	// Use this for initialization
+	void Start () {
+        // Create camera queue, with highest priority camera (main camera) first
+        cameraQueue = new Queue<Camera>();
+        cameraQueue.Enqueue(mainCamera);
+        cameraQueue.Enqueue(forwardFacingCamera);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("ChangeView"))
+        {
+            // Remove original camera from the queue and disable it
+            Debug.Log("Change Camera");
+            Camera originalCamera = cameraQueue.Peek();
+            originalCamera.enabled = false;
+            cameraQueue.Dequeue();
+
+            // Change to next camera
+            Camera nextCamera = cameraQueue.Peek();
+            Debug.Log("Next Camera: " + nextCamera);
+            nextCamera.enabled = true;
+            cameraQueue.Enqueue(originalCamera);
+        }
+    }
+}
